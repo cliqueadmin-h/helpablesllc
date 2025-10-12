d9b8ea473c013762d6525d06b128f1d175861105
Clique Admin <cliqueadmin@helpables.onmicrosoft.com>
2025-10-12 02:52:56 -0700

    Fix: create public folder and add PostgreSQL support

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,12 +1,36 @@
 // See https://docs.strapi.io/dev-docs/configurations/database
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const configs = {
+    sqlite: {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        useNullAsDefault: true,
+        debug: false,
+      },
     },
-    useNullAsDefault: true,
-    debug: false,
-  },
-});
+    postgres: {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          ssl: env.bool('DATABASE_SSL', false) && {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    },
+  };
+
+  return configs[client];
+};
