d6c357dc0ad04aec183750591f7fc009bc600200
Clique Admin <cliqueadmin@helpables.onmicrosoft.com>
2025-11-22 18:50:15 -0800

    Add Apollo tracking script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -65,6 +65,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable} dark`}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"69227426d28b6e0021a5803d"})},document.head.appendChild(o)}initApollo();`,
+          }}
+        />
+      </head>
       <body className="antialiased bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors">
         <ClientLayout>
           {children}
